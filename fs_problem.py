a760313776ff0c5701c4f67f76bef839e75eab71
3neutronstar <minsoo.kang0918@gmail.com>
2021-09-28 09:58:45 +0900

    add xgboost

--- fs_problem.py
+++ fs_problem.py
@@ -8,6 +8,7 @@ from sklearn.preprocessing import StandardScaler
 from solution import Solution
 from sklearn.linear_model import LinearRegression
 import numpy as np
+import xgboost
 class FsProblem :
     def __init__(self,typeOfAlgo,data,clinical_data,qlearn,classifier=KNeighborsClassifier(n_neighbors=1)):
         self.data=data
@@ -15,7 +16,8 @@ class FsProblem :
         self.outPuts=self.data.iloc[:,self.nb_attribs] # We initilize the labels from the last column of the dataset # 마지막 column이 정답
         self.ql = qlearn
         # self.classifier = classifier # classifier 대신에 xgboost같은거 넣으면 됨
-        self.classifier = LinearRegression()
+        # self.classifier = LinearRegression()
+        self.classifier = xgboost.XGBRegressor(n_estimators=100, learning_rate=0.08, gamma=0, subsample=0.75, colsample_bytree=1, max_depth=9)
         self.typeOfAlgo = typeOfAlgo
         self.clinical_variable_data=clinical_data.values
 
@@ -35,7 +37,7 @@ class FsProblem :
                                                     )
         self.classifier.fit(train_X,train_y)
         predict = self.classifier.predict(test_X) 
-        return 1.0/metrics.mean_squared_error(Y,predict)
+        return -metrics.mean_squared_error(Y,predict)
         # return metrics.accuracy_score(predict,test_y)
 
 
@@ -51,6 +53,6 @@ class FsProblem :
         Y = np.log(self.outPuts)
         cv = ShuffleSplit(n_splits=10, test_size=0.1, random_state=0) # Cross validation function
         # results = cross_val_score(self.classifier, X, Y, cv=cv,scoring='accuracy')
-        results = -1.0/cross_val_score(self.classifier, X, Y, cv=cv,scoring='neg_mean_squared_error')
+        results = cross_val_score(self.classifier, X, Y, cv=cv,scoring='neg_mean_squared_error')
         #print("\n[Cross validation results]\n{0}".format(results))
         return results.mean()
